fbcd788b90ae3c7014a3412abd9a243370449921
Hboni <Hboni@users.noreply.github.com>
2020-01-27 13:25:50 +0000

    Corrects compute tissues mask in spm with intracranial labels creation & PEP8
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/spm/branches/bug_fix@85976 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/spm/processes/tools/computeOneTissueMask.py
+++ brainvisa/toolboxes/spm/processes/tools/computeOneTissueMask.py
@@ -66,16 +66,21 @@ def initialization(self):
     #self.linkParameters('others_prob_maps', 'native_prob_map')  # TODO Test and take only others map differents from native_map_prob
     self.resolve_equal_probability = True
 
+
 def execution(self, context):
 
     print("Compute one tissue mask")
     prob_map_vol = aims.read(self.native_prob_map.fullPath())
     prob_map_arr = np.array(prob_map_vol, copy=False)
     prob_map_arr[prob_map_arr < 0] = 0
-
+    
+    comp_prob_map_max = []
     for other_prob_map in self.others_prob_maps:
-        comp_prob_map_arr = np.array(aims.read(other_prob_map.fullPath()), copy=False)
-        self.compare_probability_map(prob_map_arr, comp_prob_map_arr)
+        # comp_prob_map_arr = np.array(aims.read(other_prob_map.fullPath()), copy=False)
+        comp_prob_map_max.append(np.array(aims.read(other_prob_map.fullPath()), copy=False))
+        # self.compare_probability_map(prob_map_arr, comp_prob_map_arr)
+    comp_prob_map_arr = np.max(comp_prob_map_max, axis=0)
+    self.compare_probability_map(prob_map_arr, comp_prob_map_arr)
 
     prob_map_arr[prob_map_arr > 10e-5] = 1
     aims.write(prob_map_vol, self.native_mask.fullPath(), format='S16')
@@ -85,7 +90,7 @@ def compare_probability_map(self, prob_map, comp_prob_map):
 
     if self.resolve_equal_probability:
         self._resolve_equal_probability(prob_map, comp_prob_map)
-
+        
     prob_map[prob_map < comp_prob_map] = 0
 
 
@@ -102,11 +107,13 @@ def _resolve_equal_probability(self, prob_map, comp_prob_map):
         non_zero_array = prob_map != empty_arr  # Replace by np.where != 0 ?
         equal_coord = np.where(equal_arr * non_zero_array)
         for x, y, z, t in zip(equal_coord[0], equal_coord[1], equal_coord[2], equal_coord[3]):
-            if 0 < x < prob_map.shape[0] and 0 < y < prob_map.shape[1] and 0 < z < prob_map.shape[2] and 0 < t < prob_map.shape[3]:
+            if 0 <= x < prob_map.shape[0] and 0 <= y < prob_map.shape[1] and 0 <= z < prob_map.shape[2] and 0 <= t < prob_map.shape[3]:
                 neighbors_arr_mean = _computeNeighborsMean(prob_map, x, y, z, t)
                 neighbors_comp_arr_mean = _computeNeighborsMean(comp_prob_map, x, y, z, t)
                 if neighbors_arr_mean < neighbors_comp_arr_mean:
-                    prob_map[x][y][z][t] =0
+                    prob_map[x][y][z][t] = 0
+                else:
+                    prob_map[x][y][z][t] = 1
             else:
                 pass  # voxel neighbors is out of field of view
 
@@ -117,27 +124,29 @@ def _resolve_equal_probability(self, prob_map, comp_prob_map):
 def _computeNeighborsMean(arr, x, y, z, t):
     """Compute the mean value of the 18 neighbors"""
 
-    neighbors_6 = [[x-1, y, z, t],
-                   [x, y-1, z, t],
-                   [x, y, z-1, t],
-                   [x+1, y, z, t],
-                   [x, y+1, z, t],
-                   [x, y, z+1, t],
-                  ]
-    neighbors_18 = neighbors_6 +\
-                   [[x-1, y-1, z, t],
-                    [x, y-1, z-1, t],
-                    [x-1, y, z-1, t],
-                    [x+1, y+1, z, t],
-                    [x, y+1, z+1, t],
-                    [x+1, y, z+1, t],
-                    [x - 1, y + 1, z, t],
-                    [x, y - 1, z + 1, t],
-                    [x - 1, y, z + 1, t],
-                    [x + 1, y - 1, z, t],
-                    [x, y + 1, z - 1, t],
-                    [x + 1, y, z - 1, t]
-                   ]
+    neighbors_6 = [
+        [x - 1, y, z, t],
+        [x, y - 1, z, t],
+        [x, y, z - 1, t],
+        [x + 1, y, z, t],
+        [x, y + 1, z, t],
+        [x, y, z + 1, t],
+    ]
+    neighbors_18 = neighbors_6 + [
+        [x - 1, y - 1, z, t],
+        [x, y - 1, z - 1, t],
+        [x - 1, y, z - 1, t],
+        [x + 1, y + 1, z, t],
+        [x, y + 1, z + 1, t],
+        [x + 1, y, z + 1, t],
+        [x - 1, y + 1, z, t],
+        [x, y - 1, z + 1, t],
+        [x - 1, y, z + 1, t],
+        [x + 1, y - 1, z, t],
+        [x, y + 1, z - 1, t],
+        [x + 1, y, z - 1, t]
+    ]
+    
     neighbors_sum = 0
     for n in neighbors_18:
         neighbors_sum += arr[n[0]][n[1]][n[2]][n[3]]
