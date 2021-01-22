7907b1fca93a72de245400235077f6c87db138e9
Hboni <Hboni@users.noreply.github.com>
2021-01-22 14:38:23 +0000

    create spm12Reorient process
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/spm/branches/bug_fix@87285 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ brainvisa/toolboxes/spm/processes/spm12/util/SPM12Reorient_generic.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+#  This software and supporting documentation are distributed by
+#      Institut Federatif de Recherche 49
+#      CEA/NeuroSpin, Batiment 145,
+#      91191 Gif-sur-Yvette cedex
+#      France
+#
+# This software is governed by the CeCILL license version 2 under
+# French law and abiding by the rules of distribution of free software.
+# You can  use, modify and/or redistribute the software under the
+# terms of the CeCILL license version 2 as circulated by CEA, CNRS
+# and INRIA at the following URL "http://www.cecill.info".
+#
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using,  modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean  that it is complicated to manipulate,  and  that  also
+# therefore means  that it is reserved for developers  and  experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and,  more generally, to use and operate it in the
+# same conditions as regards security.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license version 2 and that you accept its terms.
+from __future__ import absolute_import
+import os
+from brainvisa.processes import Application, Signature, ListOf, ReadDiskItem, WriteDiskItem
+from brainvisa.processes import Choice, Float, String, Matrix, Boolean
+from soma.spm.spm12.util.reorient import Reorient
+from soma.spm.spm_launcher import SPM12, SPM12Standalone
+
+configuration = Application().configuration
+
+
+def validation():
+    try:
+        spm = SPM12Standalone(configuration.SPM.spm12_standalone_command,
+                              configuration.SPM.spm12_standalone_mcr_path,
+                              configuration.SPM.spm12_standalone_path)
+    except Exception:
+        spm = SPM12(configuration.SPM.spm12_path,
+                    configuration.matlab.executable,
+                    configuration.matlab.options)
+    return spm
+
+
+userLevel = 1
+name = 'spm12 - Reorient - generic'
+
+signature = Signature(
+    "images_to_reorient", ListOf(ReadDiskItem('4D Volume',
+                                              ["gz compressed NIFTI-1 image", "NIFTI-1 image"])),
+    "reorient_by", Choice(("Reorientation Matrix", 'matrix'),
+                          ("Reorientation Parameters", 'parameters'),
+                          ("Saved reorientation matrix", 'saved_matrix')),
+    "matrix", Matrix(length=4, width=4),
+    "parameters", ListOf(Float()),
+    "saved_matrix", ReadDiskItem('Matlab SPM script', 'Matlab file'),
+    "filename_prefix", String(),
+    "batch_location", WriteDiskItem('Matlab SPM script', 'Matlab script'),
+)
+
+
+def initialization(self):
+    self.setDisable('parameters', 'saved_matrix')
+    self.addLink(None, "reorient_by", self.update_reorient_method)
+    self.addLink('batch_location', 'images_to_reorient', self.update_batch_path)
+
+
+def update_reorient_method(self, proc):
+    self.setDisable('matrix', 'parameters', 'saved_matrix')
+    self.setEnable(self.reorient_by)
+    self.changeSignature(self.signature)
+
+
+def update_batch_path(self, proc):
+    """
+    Place batch file next to the first image of images_to_reorient param
+    """
+    if self.images_to_reorient:
+        image_dirname = os.path.dirname(self.images_to_reorient[0].fullPath())
+        return os.path.join(image_dirname, 'spm12_reorient_job.m')
+    else:
+        return None
+
+
+def execution(self, context):
+    reorient = Reorient()
+    
+    reorient.set_images_path_list([image.fullPath() for image in self.images_to_reorient])
+    
+    if self.reorient_by == 'matrix':
+        reorient.set_reorient_by_reorient_matrix()
+        reorient.set_reorient_matrix(list(self.matrix))
+    elif self.reorient_by == 'parameters':
+        reorient.set_reorient_by_reorient_parameters()
+        reorient.set_reorient_parameters(self.parameters)
+    elif self.reorient_by == 'saved_matrix':
+        reorient.set_reorient_by_saved_matrix()
+        reorient.set_saved_matrix(self.saved_matrix.fullPath())
+    
+    reorient.set_prefix(self.filename_prefix)
+    
+    spm = validation()
+    spm.addModuleToExecutionQueue(reorient)
+    spm.setSPMScriptPath(self.batch_location.fullPath())
+    output = spm.run()
+    context.log(name, html=output)
---
+++ python/soma/spm/spm12/util/reorient/__init__.py
@@ -0,0 +1,94 @@
+from __future__ import absolute_import
+import numpy as np
+from soma.spm.spm_main_module import SPM12MainModule
+from soma.spm.spm_batch_maker_utils import addBatchKeyWordInEachItem
+from soma.spm.custom_decorator_pattern import checkIfArgumentTypeIsAllowed, checkIfArgumentTypeIsStrOrUnicode
+from soma.spm.custom_decorator_pattern import checkIfArgumentIsInAllowedList
+
+
+class Reorient(SPM12MainModule):
+    """
+    Reorient images given a set of parameters.
+    The reorientation parameters can be given either as a 4x4 matrix or as parameters as defined for
+    spm matrix.m. The new image orientation will be computed by PRE-multiplying the original orientation
+    matrix with the supplied matrix.
+    
+    This branch contains 3 items:
+    * Images to reorient
+    * Reorient by
+    * Filename Prefix
+    """
+    
+    def __init__(self):
+        self.images_path_list = []
+        
+        self.reorient_by = 'reorient_matrix'
+        self.reorient_matrix = [[1, 0, 0, 0],
+                                [0, 1, 0, 0],
+                                [0, 0, 1, 0],
+                                [0, 0, 0, 1]]  # 4x4
+        self.reorient_parameters = [0, 0, 0, 0, 0, 0,
+                                    1, 1, 1, 0, 0, 0]  # 1x12
+        self.saved_matrix_path = ''
+        
+        self.prefix = ''
+    
+    def set_images_path_list(self, path_list):
+        if isinstance(path_list, str):
+            self.images_path_list = [path_list]
+        elif isinstance(path_list, list):
+            self.images_path_list = path_list
+        else:
+            raise ValueError("Images path has to be a list or a string")
+    
+    def set_reorient_by_reorient_matrix(self):
+        self.reorient_by = 'reorient_matrix'
+    
+    def set_reorient_by_reorient_parameters(self):
+        self.reorient_by = 'reorient_parameters'
+    
+    def set_reorient_by_saved_matrix(self):
+        self.reorient_by = 'saved_matrix'
+    
+    @checkIfArgumentTypeIsAllowed(list, 1)
+    def set_reorient_matrix(self, mat):
+        if np.array(mat).shape == (4, 4):
+            self.reorient_matrix = [[str(val) for val in line] for line in mat]
+        else:
+            raise ValueError("Reorient matrix have to be [4 x 4] matrix")
+    
+    @checkIfArgumentTypeIsAllowed(list, 1)
+    def set_reorient_parameters(self, param):
+        if np.array(param).shape == (12,):
+            self.reorient_parameters = [str(p) for p in param]
+        else:
+            raise ValueError("Reorient parameters have to be [12 x 1] array")
+
+    @checkIfArgumentTypeIsStrOrUnicode(argument_index=1)
+    def set_saved_matrix(self, path):
+        self.saved_matrix_path = path
+    
+    @checkIfArgumentTypeIsStrOrUnicode(argument_index=1)
+    def set_prefix(self, pref):
+        self.prefix = pref
+    
+    def getStringListForBatch(self):
+        batch_list = []
+        if self.images_path_list:
+            batch_im_list = ["'%s,1'" % i for i in self.images_path_list]
+            batch_list.append("spm.util.reorient.srcfiles = {%s};" % '\n'.join(batch_im_list))
+            
+            if self.reorient_by == 'reorient_matrix':
+                matrix = "\n".join([' '.join(line) for line in self.reorient_matrix])
+                batch_list.append("spm.util.reorient.transform.transM = [%s];" % matrix)
+            elif self.reorient_by == 'reorient_parameters':
+                params = " ".join(self.reorient_parameters)
+                batch_list.append("spm.util.reorient.transform.transprm = [%s];" % params)
+            elif self.reorient_by == 'saved_matrix':
+                batch_list.append("spm.util.reorient.transform.transF = {'%s'};" % self.saved_matrix_path)
+    
+            batch_list.append("spm.util.reorient.prefix = '%s';" % self.prefix)
+            
+            return batch_list
+        else:
+            raise ValueError('At least one image path is required')
